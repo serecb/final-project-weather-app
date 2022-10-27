eb4423555c8f0bf7b888d572f3864bb71e8b5f0f
serecb <112441776+serecb@users.noreply.github.com>
2022-10-27 19:25:37 +0100

    Created getTodayForecast function and made an oneCallApi that receives the coordinates to be sent to the function displayTodayForecast The  response received by this function relates to the info stored within response.data.hourly ( an array of 48 hours) In the displayTodayForecast function, I have created a variable todayForecast=response.data.hourly; and amended the html in js accordingly( to show the daily hour and temp and icon) Then I have created a formatHour with a timestamp to show the forecast for the next 24h and I have added an index to ensure this shows the result for the next 24h. Finally I have added a <hr/> to make the hourly forecast tidier

--- src/index.js
+++ src/index.js
@@ -37,28 +37,41 @@ if (minutes < 10) {
   todayDate.innerHTML = `GMT: ${hour}:0${minutes}`;
 }
 
+
+function formatHour(timeStamp){
+let date = new Date(timeStamp*1000);
+let hour=date.getHours();
+let hours=["00:00","01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00"];
+return hours[hour];
+}
+
+
 function formatDay(timeStamp){
-  let date=new Date(timeStamp * 1000);
+  let date=new Date(timeStamp * 10000);
   let day=date.getDay();
   let days=["Sun","Mon","Tue","Wed","Thur","Fri","Sat"];
-
   return days[day];
 
 }
 
-function displayTodayForecast() {
+function displayTodayForecast(response) {
+  console.log(response.data.hourly[0])
   let todayForecastElement = document.querySelector("#daily-forecast");
-  let dayHour = ["10:00", "13:00", "16:00", "19:00", "21:00", "23:00"];
+  let todayForecast=response.data.hourly;
   let todayForecastHTML = `<div class="row">`;
-  dayHour.forEach(function (hour) {
+  todayForecast.forEach(function (todayForecastHour,index) {
+    if(index<24){
     todayForecastHTML =
       todayForecastHTML +
       `<div class="col-2">
-   <div class="today-part-forecast">${hour}</div>
-   <img src="https://openweathermap.org/img/wn/03d@2x.png" alt="scattered-clouds" width="40"/>
-    <div class="max">18°C
+   <div class="today-part-forecast">${formatHour(todayForecastHour.dt)}</div>
+   <img src="https://openweathermap.org/img/wn/${
+     todayForecastHour.weather[0].icon
+   }@2x.png" alt="scattered-clouds" width="40"/>
+    <div class="max">${Math.round(todayForecastHour.temp)}°C
   </div>
-  </div>`;
+  <hr/>
+  </div>`;}
   });
   todayForecastHTML = todayForecastHTML + `</div>`;
   todayForecastElement.innerHTML = todayForecastHTML;
@@ -91,6 +104,12 @@ function displayWeeklyForecast(response) {
 }
 
 
+function getTodayForecast(coordinates){
+   let apiKey = "faa50e274cdcc1720b61bb86d2823360";
+   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+   axios.get(apiUrl).then(displayTodayForecast);
+}
+
 function getWeeklyForecast(coordinates) {
   let apiKey = "faa50e274cdcc1720b61bb86d2823360";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
@@ -116,6 +135,7 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
   getWeeklyForecast(response.data.coord);
+  getTodayForecast(response.data.coord);
 }
 
 function search(city) {
@@ -154,7 +174,6 @@ let celsiusTempLink = document.querySelector("#celsius");
 celsiusTempLink.addEventListener("click", displayCelsiusTemp);
 
 search("Madrid");
-displayTodayForecast();
 
 function showPosition(position) {
   let apiKey = "5faa50e274cdcc1720b61bb86d2823360";
