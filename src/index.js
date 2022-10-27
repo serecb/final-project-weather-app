21516ff227bda6f81902ce0926b733f22628ab31
serecb <112441776+serecb@users.noreply.github.com>
2022-10-27 14:23:26 +0100

    console.logged the response.data.daily of the function displayWeeklyForecast and added the units=metric to the apiUrl or the function getWeeklyForecast

--- src/index.js
+++ src/index.js
@@ -56,7 +56,6 @@ function displayTodayForecast() {
 }
 
 function displayWeeklyForecast(response) {
-  console.log(response.data.daily);
   let weeklyForecastElement = document.querySelector(
     "#weekly-weather-forecast"
   );
@@ -79,9 +78,10 @@ function displayWeeklyForecast(response) {
   weeklyForecastElement.innerHTML = weeklyForecastHTML;
 }
 
+
 function getWeeklyForecast(coordinates) {
   let apiKey = "faa50e274cdcc1720b61bb86d2823360";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeeklyForecast);
 }
 
